7a10745951d892e73c14fa2cd61713344f0bc1ab
Coder3451 <segniwoldemichael@gmail.com>
2024-07-29 10:46:43 -0700

    Putting everyting as before

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
 int _printf(const char *format, ...);
 
 void wstring(char *s, int *chnum);
 void wchar(char c, int *chnum);
-#endif
 
+#endif
--- tests/main.h
+++ tests/main.h
@@ -1,13 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
 int _printf(const char *format, ...);
 
 void wstring(char *s, int *chnum);
 void wchar(char c, int *chnum);
 
 #endif
-<<<<<<< HEAD
-=======
-
->>>>>>> aa6f29656bc4eff0afd64a34c2f4a2f35cafcb09
